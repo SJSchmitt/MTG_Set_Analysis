37ad18b2c3841f47e7d6fc641201111c0d04d183
Sam Schmitt <sjschmitt13@gmail.com>
2023-07-24 21:58:35 -0500

    scrape all standard sets and store results in PostgreSQL

--- set_scrape.py
+++ set_scrape.py
@@ -3,9 +3,13 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
+# import pandas
+import pandas as pd
+
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
+# function to scrape a list of all cards in set and links to those cards
 def scrapeCardList(setLink):
     browser.visit(setLink)
     # create empty lists
@@ -13,19 +17,27 @@ def scrapeCardList(setLink):
     cardNames = []
     # link to more card details
     cardLinks = []
+    releaseDate = []
 
     # gather html from url
     html = browser.html
     setSoup = soup(html, 'html.parser')
+
+    # get the release date
+    dateRows = setSoup.select('p.set-header-title-subline')
+    release = dateRows[0].text.split('\xa0')[-1].strip()
 
     # scrape card names and links
     rows = setSoup.select('a.card-grid-item-card')
     for row in rows:
         cardNames.append(row.text.strip())
         cardLinks.append(row['href'])
+        releaseDate.append(release)
 
-    return cardNames, cardLinks
+    return cardNames, cardLinks, releaseDate
 
+# function to scrape further card details
+# mana costs, cmc, colors, card type, subtype, and supertype, rarity, power, and toughness
 def scrapeCards(cardLinks, setAbbr):
     # create empty lists
     manaCosts = []
@@ -35,6 +47,7 @@ def scrapeCards(cardLinks, setAbbr):
     black = []
     red = []
     green = []
+    superType = []
     cardType = []
     subtype = []
     rarity = []
@@ -68,13 +81,32 @@ def scrapeCards(cardLinks, setAbbr):
         red.append('1') if 'R' in manaCost else red.append('0') 
         green.append('1') if 'G' in manaCost else green.append('0')
         
-        # card type and subtype
-        fullType = cardSoup.select('p.card-text-type-line')[0].text.strip().split(' — ')
-        cardType.append(fullType[0])
-        if len(fullType) == 1:
+        # card type, subtype, and supertype
+        fullType = cardSoup.select('p.card-text-type-line')[0].text.strip()
+        super = ''
+        if 'Snow ' in fullType:
+            super += 'Snow '
+            fullType = fullType.replace('Snow ', '')
+        if 'Legendary ' in fullType:
+            super += 'Legendary '
+            fullType = fullType.replace('Legendary ', '')
+        if 'Ongoing ' in fullType:
+            super += 'Ongoing '
+            fullType = fullType.replace('Ongoing ', '')
+        if 'Snow ' in fullType:
+            super += 'Snow '
+            fullType = fullType.replace('Snow ', '')
+        if 'World ' in fullType: 
+            super += 'World'
+            fullType = fullType.replace('World ', '')
+        superType.append(super)
+
+        mainType = fullType.split(' — ')
+        cardType.append(mainType[0])
+        if len(mainType) == 1:
             subtype.append('')
         else:
-            subtype.append(fullType[1])
+            subtype.append(mainType[1])
         
         # rarity
         rarity.append(cardSoup.select('span.prints-current-set-details')[0].text.split(' · ')[1])
@@ -93,4 +125,18 @@ def scrapeCards(cardLinks, setAbbr):
         
         # set
         cardSet.append(setAbbr)
-    return manaCosts, cmc, cardType, subtype, rarity, power, toughness, cardSet, white, blue, black, red, green
+    return manaCosts, cmc, superType, cardType, subtype, rarity, power, toughness, cardSet, white, blue, black, red, green
+
+def setScrape(setAbbr):
+    setLink = "https://scryfall.com/sets/" + setAbbr
+    # scrape card names and links for the set
+    names, cardLinks, release = scrapeCardList(setLink)
+    # scrape card details
+    manaCosts, cmc, superType, cardType, subtype, rarity, power, toughness, cardSet, white, blue, black, red, green = scrapeCards(cardLinks, setAbbr)
+    df = pd.DataFrame({'Card': names, 'Set': cardSet, 'Release': release, 'Supertype': superType, 'Type': cardType, 'Subtype': subtype, 'Mana_Cost': manaCosts, 
+                    'CMC': cmc, 'Power': power, 'Toughness': toughness, 'Rarity': rarity, 
+                   'White': white, 'Blue': blue, 'Black': black, 'Red': red, 'Green': green, 'Link': cardLinks})
+    
+    newDf = df.drop_duplicates(subset=['Card'])
+    return newDf
+
